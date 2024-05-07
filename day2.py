2f7dbd22774b7b5681129c7c7c52f202c93b3374
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-05-07 16:30:55 +0530

    update

---
+++ day2.py
@@ -0,0 +1,28 @@
+"""Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird."""
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n % 2 == 1:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+elif n % 2 == 0 and n > 20:
+    print("Not Weird")

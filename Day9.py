0a1905b2991a66ef4209941abc38acd25e8a2a4b
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-05-11 21:19:47 +0530

    update

--- Day9.py
+++ Day9.py
@@ -1,5 +1,5 @@
 # Description: Write a function that takes a year as a parameter and returns True if the year is a leap year, False otherwise.
-# if a year divide by 4 and 100 then year also must be divide by 400 
+# if a year divide by 4 and 100 then year also must be divide by 400
 
 def is_leap(year): # created a function 
     if year % 4 == 0: # 

295d07af60ae428247ae71018fccb51184415c63
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-05-21 20:00:57 +0530

    update

--- day17Mean,Median,Mode.py
+++ day17Mean,Median,Mode.py
@@ -17,4 +17,31 @@ mode = min(modes) #get the smallest element with the maximum frequency in the li
 print( mode) #print the mode
 
     
+
+#weighted mean 
+#https://www.hackerrank.com/challenges/s10-weighted-mean/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the 'weightedMean' function below
+
+def weightedMean(X, W):
+    weighted_sum = sum([X[i] * W[i] for i in range(len(X))])
+    sum_weights = sum(W)
+    return weighted_sum / sum_weights
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    X = list(map(int, input().rstrip().split()))
+
+    W = list(map(int, input().rstrip().split()))
+
+    result = weightedMean(X, W)
+    print("{:.1f}".format(result))
+
     

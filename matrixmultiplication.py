959c516c58efe81154f8d39e4d8b6a5612d6e20b
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-12 19:54:50 +0530

    update

--- matrixmultiplication.py
+++ matrixmultiplication.py
@@ -5,3 +5,16 @@
 #i need to multiply the elements of the two matrices
 #i need to add the products of the two matrices
 #i need to store the result in the ith row and jth column of the resultant matrix
+#c=[[0,0,0],[0,0,0],[0,0,0]]
+#print(c)
+#c=[[],[],[]] #this is a list of lists
+#c[0].append(0) adding 0 to the first list of c 
+#output= [[[0,0,0],[],[]]] it gives 0 to the first list of c 
+#now i need to add 0 to the second and third list of c using for loop 
+def initialize_mat(dim):
+    c=[]
+    for i in range(dim):
+        c.append([])
+    for i in range(dim):
+        c[i].append(0)
+    return c

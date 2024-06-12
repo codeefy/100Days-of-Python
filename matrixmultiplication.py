79e77d7ede0face973dc4cdde6ac37a0ff391e1a
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-12 19:47:08 +0530

    update

---
+++ matrixmultiplication.py
@@ -0,0 +1,7 @@
+# intialise C to Zero
+# i need to consider two matrices A and B 
+#I need to find the product of the two matrices
+#i nedd to pick ith row of A and jth column of B
+#i need to multiply the elements of the two matrices
+#i need to add the products of the two matrices
+#i need to store the result in the ith row and jth column of the resultant matrix

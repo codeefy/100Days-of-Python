471bc3cf030908affd544b54936819d56ec9644a
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-13 18:14:44 +0530

    update

--- recursion.py
+++ recursion.py
@@ -13,58 +13,9 @@ def count_lowercase(s):
         if i.islower():
             count+=1
     return count
-def count
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def count_uppercase(s):
-    count=0
-    for i in s:
-        if i.isupper():
-            count+=1
-    return count
-def count_lowercase(s):
-    count=0
-    for i in s:
-        if i.islower():
-            count+=1
-    return count
 def count_characters(s):
     count=0
-    for i in s:
+    for i in s :
         count+=1
     return count
 def count_words(s):
@@ -72,9 +23,9 @@ def count_words(s):
     for i in s:
         if i==" ":
             count+=1
-    return count+1 
+    return count+1
+
 def count_all(s):
     return count_uppercase(s),count_lowercase(s),count_characters(s),count_words(s)
 s="FUNCTIONS have to be Defined before THEY can be called. the function Call Cannot come before the DEFINITION"
 print(count_all(s))
- 

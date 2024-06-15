d7883ddecd7f50872ea4c59d920c846e827e6a04
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-15 17:13:03 +0530

    update

--- The Minion game .py
+++ The Minion game .py
@@ -6,6 +6,19 @@
 # Stuart has to make words starting with consonants.
 # Kevin has to make words starting with vowels.
 # The game ends when both players have made all possible substrings.
+# Scoring
+# A player gets +1 point for each occurrence of the substring in the string .
+# For Example:
+# String  = BANANA
+# Kevin's vowel beginning word = ANA
+# Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+# For better understanding, see the image below:
+# banana.png
+# Your task is to determine the winner of the game and their score.
+#https://www.hackerrank.com/challenges/the-minion-game/problem?isFullScreen=false
+#Input Format
+# A single line of input containing the string .
+# Note: The string  will contain only uppercase letters: 
 def minion_game(string):
     # your code goes here
     vowels = 'AEIOU'

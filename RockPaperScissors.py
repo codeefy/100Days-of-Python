2221818993af08bb25feecd5d44e0dd8cb725392
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-17 22:53:21 +0530

    update

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,1 +1,9 @@
 # write code for rock paper and scissior game using python 
+# rock beats scissor
+# scissor beats paper
+# paper beats rock
+# take input from the user
+# generate a random number
+# compare the random number with the user input 
+# declare the winner
+# print the result

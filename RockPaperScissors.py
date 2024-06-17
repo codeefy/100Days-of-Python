ff7ea1cda0f6a17d000a6982c07f71cace0ee748
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-17 22:53:49 +0530

    update

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,8 +2,3 @@
 # rock beats scissor
 # scissor beats paper
 # paper beats rock
-# take input from the user
-# generate a random number
-# compare the random number with the user input 
-# declare the winner
-# print the result

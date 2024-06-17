d879485e5f912d61c2783d639ee263638a0f6f80
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-17 22:55:41 +0530

    update

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,3 +2,8 @@
 # rock beats scissor
 # scissor beats paper
 # paper beats rock
+#The player can choose one of the three options rock, paper, or scissors and should be warned if they enter an invalid option.
+#At each round, the player must enter one of the options in the list and be informed if they won, lost, or tied with the opponent.
+#By the end of each round, the player can choose whether to play again.
+#Display the player's score at the end of the game.
+#The minigame must handle user inputs, putting them in lowercase and informing the user if the option is invalid.

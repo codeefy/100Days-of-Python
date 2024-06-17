d69e9a29caafa3e5481028e736407ea5c4f9abbb
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-17 23:59:10 +0530

    update

--- FavouriteSingerProblem.py
+++ FavouriteSingerProblem.py
@@ -1,9 +1,9 @@
 name = int(input())  # get the input from the user  
 playlist = input().split(" ")  # get the input from the user and split it by space  
 
-def count_favourite_singers(playlist): 
-    singer_count = {} 
-    for singer in playlist:
+def count_favourite_singers(playlist):  #function to count the favourite singers in the playlist 
+    singer_count = {}  #initialise an empty dictionary  
+    for singer in playlist: #for each singer in the playlist 
         if singer in singer_count:             
             singer_count[singer] += 1         
         else:             

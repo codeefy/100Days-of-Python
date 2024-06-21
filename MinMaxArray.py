d5acbf76e5d5bf194332f79b224166690e5e6cf5
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-21 21:40:56 +0530

    update

--- MinMaxArray.py
+++ MinMaxArray.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 def task(arr):
-        np_array = np.array(arr)
-        min_array = np.min(np_array, axis=1)
-        return np.max(min_array)
+        np_array = np.array(arr) # it is a 2D array with n rows and m columns   
+        min_array = np.min(np_array, axis=1) #min of each row in the 2D array 
+        return np.max(min_array) #max of the min of each row in the 2D array 
 
 if __name__ == '__main__':
         arr = []

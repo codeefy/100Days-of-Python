aea8a17bf40f60e6ab8043cffeff28d1143a5f2a
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-21 21:13:30 +0530

    update

--- arrayexp.py
+++ arrayexp.py
@@ -1,13 +1,15 @@
+# reverse the array and convert it into float array using numpy 
+
 import numpy
 
-def arrays(arr):
+def arrays(arr):  #arr is a list of integers separated by space  
     # complete this function
     # use numpy.array
-    arr = list(map(float, arr))
-    arr.reverse()
+    arr = list(map(float, arr)) #convert the list of integers to a list of floats  
+    arr.reverse() #reverse the list of floats 
     
-    return numpy.array(arr, float)
+    return numpy.array(arr, float) #return the reversed list of floats as a numpy array of floats 
 
-arr = input().strip().split(' ')
+arr = input().strip().split(' ') #input is a list of integers separated by space    
 result = arrays(arr)
 print(result)

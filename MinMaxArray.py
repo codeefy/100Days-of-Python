e0e11f2243d65c0c977229cb750961a49cdd0b97
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-21 21:42:07 +0530

    update

--- MinMaxArray.py
+++ MinMaxArray.py
@@ -5,11 +5,12 @@ def task(arr):
         min_array = np.min(np_array, axis=1) #min of each row in the 2D array 
         return np.max(min_array) #max of the min of each row in the 2D array 
 
-if __name__ == '__main__':
-        arr = []
-        n, m = list(map(int, input().split()))
-        arr.append([n, m])
-        for i in range(n):
-                arr.append(list(map(int, input().split())))
-        result = task(arr)
+if __name__ == '__main__': # main function to take input from the user and print the output 
+        arr = [] # list to store the input values  
+        n, m = list(map(int, input().split())) # taking the input values of n and m seperated by space 
+        arr.append([n, m]) # appending the values of n and m to the list 
+        for i in range(n): # for loop to take the input values of the 2D array
+                arr.append(list(map(int, input().split()))) # appending the input values of the 2D array to the list
+        result = task(arr) # calling the function task with the input values
+    
         print(result)

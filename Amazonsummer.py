5cadb243188880962d954ef23a5b54dce9a6055c
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-23 02:54:22 +0530

    update

---
+++ Amazonsummer.py
@@ -0,0 +1,10 @@
+#There are three robots named Ray, Ben and Kevin. Initially Ray has a string S of length N.
+#while the other two robots have empty strings. We can make either of the following moves:
+#Move |: Remove the first character from Ray’s string and append it to Ben’s string.
+#Move 2:Remove the last character from Ben’s string and append it to Kvin’s string.
+#You must perform either of the two moves mentioned above in such a way that the strings left
+#with Ray and Ben are empty and the string left with Kevin is lexicographically the smallest.
+#Your task is to return this lexicographically smallest string that Kevin has after completing this activity.
+#Note: For any two given strings, a string is said to be lexicographically smaller than the other
+#if it comes before the other string in the dictionary
+

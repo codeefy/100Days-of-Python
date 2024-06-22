3bd60a0646b44a24d766ca457750b49bfd4d23bf
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-22 22:14:40 +0530

    update

--- TrappingRainwater2.py
+++ TrappingRainwater2.py
@@ -1,4 +1,18 @@
-class Solution:
+# Link: https://practice.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1
+# Title: Trapping Rain Water
+
+
+# Trapping Rain Water
+# Given an array arr[] of N non-negative integers representing the height of blocks. If width of each block is 1, compute how much water can be trapped between the blocks during the rainy season.
+# Example 1:
+# Input:
+# N = 6
+# arr[] = {3,0,0,2,0,4}
+# Output:
+# 10
+# Explanation: The heights are 3 0 0 2 0 4. So we can trap "3*2 units of water" between the 3 and 2, "1 unit" between the 2 and 0 and "3 units" between the 2 and 4. Hence, the total unit of water trapped is 10.
+
+class Solution: 
     def trappingWater(self, arr, n):
         # Two-pointer approach
         if not arr:

936526774133212def7c192294503b37087644f2
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-24 18:42:06 +0530

    update

---
+++ RangoliAlphabet.py
@@ -0,0 +1,30 @@
+# You are given an integer,Your task is to print an alphabet rangoli of size 
+# N. (Rangoli is a form of Indian folk art based on creation of patterns.)
+# Different sizes of alphabet rangoli are shown below:
+# size 5
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
+#approach:
+# 1. First we need to create a list of alphabets from a to z.
+# 2. Then we need to create a list of alphabets from a to z in reverse order.
+
+
+import string
+def print_rangoli(size):
+    alphabets = string.ascii_lowercase # list of alphabets from a to z   
+    n = size
+    x = list(range(n))
+    x = x[:-1]+x[::-1] # list of alphabets from a to z in reverse order x[:-1] is used to remove the last element from the list x[::-1] is used to reverse the list x
+    for i in x:
+        print('-'.join(alphabets[n-1:i:-1]+alphabets[i:n]).center(n*4-3, '-')) # printing the alphabets in the required format using join and center function of python 
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)

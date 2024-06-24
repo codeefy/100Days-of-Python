53105887da8f7cb988dddcca5067411822621bf1
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-24 19:07:24 +0530

    update

--- RangoliAlphabet.py
+++ RangoliAlphabet.py
@@ -17,3 +17,17 @@ import string
 # 1. First we need to create a list of alphabets from a to z.
 # 2. Then we need to create a list of alphabets from a to z in reverse order.
 
+#code starts here
+def print_rangoli(size):
+    # your code here
+    alphabets = list(string.ascii_lowercase)
+    rangoli = []
+    for i in range(size):
+        s = '-'.join(alphabets[size-1:i:-1] + alphabets[i:size])
+        rangoli.append((s.center(size*4-3, '-')))
+    print('\n'.join(rangoli[::-1] + rangoli[1:]))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
+

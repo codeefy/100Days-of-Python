0e5d2ce4489a56764257eae1b181363d3cffcdc1
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-06-25 16:03:57 +0530

    update

--- DistinctK.py
+++ DistinctK.py
@@ -9,9 +9,22 @@
 #The next N lines contain strings.
 #The next line contains an integer k.
 
+#Output Format: The output contains the kth distinct string. If there are less than k unique string display an empty string.
+
 #approach
 #1. Read the input values
 #2. Create a set to store the unique strings
 #3. Iterate through the strings and add the strings to the set
 #4. If the length of the set is less than k, print an empty string
 #5. Else print the kth element of the set
+
+#Example
+#Input
+#6
+#d
+#b
+#c
+#b
+#c
+#a
+#2

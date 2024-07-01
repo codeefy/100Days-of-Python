e1fe45229858ad0f21b3737194eaa56f3087a6bc
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-07-01 17:49:28 +0530

    update

---
+++ RepeatedString.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/repeated-string/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+# In this problem, we are given a string s and an integer n. We have to find the number of occurrences of 'a' in the string s repeated n times.
+#  For example, if s = 'abcac' and n = 10, the string s will be repeated 10 times to form a new string 'abcacabcacabcacabcacabcacabcacabcacabcacabcacabcacabcacabcac'.
+#  The number of occurrences of 'a' in the new string is 4. So, the output will be 4.
+
+# The idea is to find the number of occurrences of 'a' in the string s and then multiply it by 
+# the number of times the string s is repeated in the new string.

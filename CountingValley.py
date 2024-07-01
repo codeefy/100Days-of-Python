00fe6bd9e389de2a8f21f8299020b198b0742adc
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-07-01 17:16:56 +0530

    update

--- CountingValley.py
+++ CountingValley.py
@@ -1,2 +1,21 @@
 #https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 
+def countingValleys(n, s):
+    # Write your code here
+    level = 0
+    valleys = 0
+    for step in s:
+        if step == 'U':
+            level += 1
+            if level == 0:
+                valleys += 1
+        else:
+            level -= 1
+    return valleys
+
+n = int(input())
+s = input()
+
+print(countingValleys(n, s))
+
+

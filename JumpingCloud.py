0a5b7160666cd11fe0c2acc76f7d17ce63311663
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-07-02 23:56:06 +0530

    update

--- JumpingCloud.py
+++ JumpingCloud.py
@@ -6,7 +6,7 @@ import random
 import re
 import sys
 
-#
+#https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 # Complete the 'jumpingOnClouds' function below.
 #
 # The function is expected to return an INTEGER.

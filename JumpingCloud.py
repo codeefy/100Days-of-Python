72d3457775761672a3d1b846929c60711dec2274
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-07-02 23:57:21 +0530

    update

--- JumpingCloud.py
+++ JumpingCloud.py
@@ -10,6 +10,23 @@ import sys
 # Complete the 'jumpingOnClouds' function below.
 # The function is expected to return an INTEGER.
 # The function accepts INTEGER_ARRAY c as parameter.
+# In this problem, we are given an array c of integers where each element represents a cloud.
+#  The value of each element can be 0 or 1. The value 0 represents a safe cloud, and the value 1 represents a thundercloud.
+#  We have to find the minimum number of jumps required to reach the end of the array.
+#  We can jump from one cloud to another if the difference between the indices of the two clouds is 1 or 2.
+#  For example, if the array c is [0, 1, 0, 0, 0, 1, 0], then the minimum number of jumps required is 3.
+#  The possible jumps are 0 -> 2 -> 3 -> 4 -> 6.
+#  So, the output will be 3.
+#  The idea is to start from the first cloud and check if we can jump 2 clouds ahead.
+#  If we can jump 2 clouds ahead, then we jump 2 clouds ahead and call the function recursively with the remaining clouds.
+#  If we cannot jump 2 clouds ahead, then we jump 1 cloud ahead and call the function recursively with the remaining clouds.
+#  If we reach the end of the array, then we return 0.
+#  If we reach the second last cloud, then we return 1.
+#  If we reach the third last cloud, then we return 1.
+#  If we reach the fourth last cloud, then we return 2.
+#  If we reach the fifth last cloud, then we return 2.
+#  If we reach the sixth last cloud, then we return 3.
+
 
 
 def jumpingOnClouds(c):

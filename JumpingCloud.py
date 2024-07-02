fcb25b0221786040d9923adafe6f004d64aab0f1
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-07-02 23:55:31 +0530

    update

---
+++ JumpingCloud.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    if len(c) == 1:
+        return 0
+    if len(c) == 2:
+        return 1
+    if c[2] != 1:
+        return jumpingOnClouds(c[2:]) + 1
+    else:
+        return jumpingOnClouds(c[1:]) + 1
+
+if __name__ == '__main__':
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+   
